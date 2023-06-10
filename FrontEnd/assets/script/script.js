3f0bee169ef044a7102fac085b566ab61fafc06a
heltnx <helenetnx@yahoo.fr>
2023-06-10 10:23:39 +0200

    ajout commentaies explicatifs dans fichier js

--- FrontEnd/assets/script/script.js
+++ FrontEnd/assets/script/script.js
@@ -1,26 +1,40 @@
-let works;
+
+/*   INFO   "await" est utilisé pour attendre que 
+  la requête soit résolue à chaque étapes 
+  il ne peut être utilisé que dans une fonction "async" */
+
+/* fonction pour récupérer les données */
 
 async function getworks(){
-    const reponse = await fetch("http://localhost:5678/api/works");
-    works = await reponse.json();
+
+    const reponse = await fetch("http://localhost:5678/api/works"); // Envoi une requête GET à l'API pour récupérer les données
+    
+    works = await reponse.json(); // Conversion de la réponse en format JSON et stockage dans la variable 'works'
 }
 
-/** remplir la gallery */
+/**--- remplir la galerie ----*/
 
-const gallery = document.querySelector(".gallery");
+// Sélection du 1er élément HTML de la class 'gallery'
+
+const gallery = document.querySelector(".gallery"); 
+
+// fonction pour afficher les données dans la galerie
 
 async function showWorks(){ 
-    
-    await getworks();
+   
+    await getworks();  // Appel de la fonction 'getworks' pour récupérer les données
 
-works.forEach(element => {
-    gallery.innerHTML += 
-    `<figure>
-    <img src="${element.imageUrl}" alt="${element.title}">
-    <figcaption>${element.title}</figcaption>
-</figure>` ;
-});
-
+   
+    works.forEach(element => {  // Parcours de chaque élément dans le tableau 'works'
+       
+        gallery.innerHTML +=  // Génére le contenu HTML pour chaque element
+        `<figure>
+            <img src="${element.imageUrl}" alt="${element.title}">
+            <figcaption>${element.title}</figcaption>
+        </figure>` ;
+    });
 }
+
+// Appel de la fonction 'showWorks' pour afficher les donnees dans la galerie
 
 showWorks();
