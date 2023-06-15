06a735a4dfded15433c4bbddb5ef8d3504bdf738
heltnx <helenetnx@yahoo.fr>
2023-06-15 17:39:27 +0200

    ajout close fenetre modale

--- FrontEnd/assets/script/modal.js
+++ FrontEnd/assets/script/modal.js
@@ -1,10 +1,35 @@
+let modal = null;
+
 const open_modal = function(event){
-    event.preventDefault()
-    const target = document.querySelector(event.target.getAttribute('href')) 
-    target.style.display = null
-    target.setAtrribute('aria-hidden', false )
-    target.setAtrribute('aria-modal', true )
+    event.preventDefault();
+    const target = document.querySelector(event.target.getAttribute('href'));
+    const target_index = document.querySelector(event.target.getAttribute('href'));
+    target.style.display = null;
+    target_index.style.zIndex = "2";
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', true);
+    modal = target;
+    modal.addEventListener('click', close_modal);
+    modal.querySelector('.js-modal-close').addEventListener('click', close_modal);
 }
+
+const close_modal = function(event){
+    if (modal === null) return ;
+    event.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute('aria-hidden', true);
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', close_modal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', close_modal);
+    modal = null;
+}
+
 document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener("click", open_modal)
-})
+    a.addEventListener("click", open_modal);
+});
+
+
+
+
+
+
