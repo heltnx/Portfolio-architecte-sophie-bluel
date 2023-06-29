ac12d78fbb0e231d591e585d05f85d1664201cb0
heltnx <helenetnx@yahoo.fr>
2023-06-29 17:19:31 +0200

    reagencement du code pour open et close modales

--- FrontEnd/assets/script/modal.js
+++ FrontEnd/assets/script/modal.js
@@ -1,49 +1,7 @@
 
-/*---------- gestion ouverture fermetures modales container------------------------*/
+/*---------- gestion ouverture fermetures modales ------------------------*/
 
 let modal = null; // initialise la variable 'modal' à null (fermée)
-
-// Fonction pour ouvrir la modale
-const open_modal = function (event) {
-    event.preventDefault();
-    const target = document.querySelector(event.target.getAttribute('href')); // selectionne la modale "cible" à partir de l'attribut href du lien cliqué (#modal1)
-    target.style.display = null; //definit la modale avec une valeur par défaut à "null" (enlève le "display none")
-    target.style.zIndex = "2"; // Définit un index z pour placer la modale au-dessus des autres éléments de la page
-    target.removeAttribute('aria-hidden'); // Supprime aria-hidden pour rendre la modale accessible aux lecteurs d'écran
-    target.setAttribute('aria-modal', true); // Ajoute aria-modal = true pour indiquer qu'il s'agit d'une modale
-
-    modal = target; // Attribue la modale actuelle à la variable 'modal'
-
-    modal.addEventListener('click', close_modal); // au click "fermer la modale" 
-
-    modal.querySelector('.js-modal-close').addEventListener('click', close_modal); // au click sur le bouton "fermer la modale"
-
-    modal.querySelector('.modal-contain').addEventListener('click', function (event) {
-        event.stopPropagation(); // au click dans le conteneur de la modale
-
-        event.stopPropagation(); // Arrête la propagation du click "fermer la modale" à l'interieur de la modale
-    });
-}
-
-// Fonction pour "fermer la modale"
-const close_modal = function (event) {
-    if (modal === null) return; // Si la variable 'modal' est null, elle est déjà fermée, on ne fait rien.
-    event.preventDefault();
-    modal.style.display = "none"; // rend la modale invisible
-    modal.setAttribute('aria-hidden', true); // Ajoute aria-hidden = true pour cacher la modale aux lecteurs d'écran
-    modal.removeAttribute('aria-modal'); // Supprime l'attribut aria-modal pour indiquer que la modale n'est plus active
-    modal.removeEventListener('click', close_modal); // Supprime le click pour "fermer la modale"
-    modal.querySelector('.js-modal-close').removeEventListener('click', close_modal); // Supprime le click pour "fermer la modale" sur le bouton
-    modal = null; // Réinitialise la variable 'modal' à null
-    window.location.reload();
-}
-
-/* action ouvrir la modale au click sur un lien des elements ayant cette class ".js-modal" */
-document.querySelectorAll('.js-modal').forEach(a => { // Sélectionne tous les éléments avec la classe 'js-modal'
-    a.addEventListener("click", open_modal);
-});
-
-/*------------- gestion ouverture et fermeture contenu des 2 modales -----------------------------------*/
 
 //déclaration des variables
 const divModalGallery = document.getElementById("galleryPhoto"); // modale 1 gallery
@@ -52,34 +10,81 @@ const fleche_retour = document.getElementById("retour"); // fleche retour
 
 // Fonction pour ajouter une classe d'un élément
 function addClass(element, className) {
-    element.classList.add(className);
+  element.classList.add(className);
 }
 // Fonction pour supprimer une classe d'un élément
 function removeClass(element, className) {
-    element.classList.remove(className);
+  element.classList.remove(className);
 }
 
-/*---------gestionnaire d'évènements au click sur la modale--------- */
+// Fonction pour ouvrir la modale "gallery"
+function opengallery() {
+  addClass(divModalAjout, "hidden"); // cache le contenu de la modale "ajout"
+  removeClass(divModalAjout, "active"); // supprime la class active
+  removeClass(divModalGallery, "hidden"); // supprime le "display none"
+  addClass(divModalGallery, "active"); // Ajoute le contenu de la modale "gallery"
+}
+
+// Fonction pour ouvrir la modale "ajout"
+function openform() {
+  addClass(divModalGallery, "hidden"); // cache le contenu de la modale "gallery"
+  removeClass(divModalGallery, "active"); // supprime la class active
+  addClass(divModalAjout, "active"); // Ajoute le contenu de la modale "ajout"
+  removeClass(divModalAjout, "hidden"); // supprime le "display none"
+}
+
+// Fonction pour ouvrir la modale "principale" au click sur "modifier"
+const open_modal = function (event) {
+  event.preventDefault();
+  const target = document.querySelector(event.target.getAttribute('href')); // selectionne la modale "cible" à partir de l'attribut href du lien cliqué (#modal1)
+  target.style.display = null; //definit la modale avec une valeur par défaut à "null" (enlève le "display none")
+  target.style.zIndex = "2"; // Définit un index z pour placer la modale au-dessus des autres éléments de la page
+  target.removeAttribute('aria-hidden'); // Supprime aria-hidden pour rendre la modale accessible aux lecteurs d'écran
+  target.setAttribute('aria-modal', true); // Ajoute aria-modal = true pour indiquer qu'il s'agit d'une modale
+  
+  opengallery();
+
+  modal = target; // Attribue la modale actuelle à la variable 'modal'
+
+  modal.addEventListener('click', close_modal); // au click "fermer la modale" 
+
+  modal.querySelector('.js-modal-close').addEventListener('click', close_modal); // au click sur le bouton "fermer la modale"
+
+  // Arrête la propagation du click "fermer la modale" à l'interieur de la modale
+  modal.querySelector('.modal-contain').addEventListener('click', function (event) {
+    event.stopPropagation(); 
+  });
+}
+
+// Fonction pour "fermer la modale"
+const close_modal = function (event) {
+  if (modal === null) return; // Si la variable 'modal' est null, elle est déjà fermée, on ne fait rien.
+  event.preventDefault();
+  modal.style.display = "none"; // rend la modale invisible
+  modal.setAttribute('aria-hidden', true); // Ajoute aria-hidden = true pour cacher la modale aux lecteurs d'écran
+  modal.removeAttribute('aria-modal'); // Supprime l'attribut aria-modal pour indiquer que la modale n'est plus active
+  modal.removeEventListener('click', close_modal); // Supprime le click pour "fermer la modale"
+  modal.querySelector('.js-modal-close').removeEventListener('click', close_modal); // Supprime le click pour "fermer la modale" sur le bouton
+  modal = null; // Réinitialise la variable 'modal' à null
+}
+
+/* ouvrir la modale au click sur un lien des elements ayant cette class ".js-modal" */
+document.querySelectorAll('.js-modal').forEach(a => { // Sélectionne tous les éléments avec la classe 'js-modal'
+  a.addEventListener("click", open_modal);
+});
+
 
 // click sur le bouton "Ajouter" dans la première modale
 document.getElementById("ajouter").addEventListener("click", function () {
-    addClass(divModalGallery, "hidden"); // cache le premier contenu de la modale
-    removeClass(divModalGallery, "active"); // supprime la class active
-    addClass(divModalAjout, "active"); // Ajoute la deuxième contenu de la modale
-    removeClass(divModalAjout, "hidden"); // supprime la class hidden
-   
+  openform();
 });
 
 // click sur la flêche "retour" de la deuxieme modale
-const modalAjout = document.getElementById("ajout")
-modalAjout.addEventListener("click", function (event) {
-    if (event.target.id === "retour") {
-        addClass(divModalAjout, "hidden"); // cache le deuxième contenu de la modale
-        removeClass(divModalAjout, "active"); // supprime la class active
-        removeClass(divModalGallery, "hidden"); // supprime la class hidden
-        addClass(divModalGallery, "active"); // Ajoute le premier contenu de la modale
-    }
+document.getElementById("retour").addEventListener("click", function () {
+  opengallery();
 });
+
+
 
 /*-------- rempli dynamiquement la modale Gallery Photo-----------------*/
 
